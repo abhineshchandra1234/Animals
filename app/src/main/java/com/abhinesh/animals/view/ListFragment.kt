93cc5b3527302e4b292f0d5452544ba0fc142ed3
Abhinesh Chandra <abhineshchandra1234@gmail.com>
2020-10-20 18:28:35 +0530

    second commit!

--- app/src/main/java/com/abhinesh/animals/view/ListFragment.kt
+++ app/src/main/java/com/abhinesh/animals/view/ListFragment.kt
@@ -5,11 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import com.abhinesh.animals.R
+import com.abhinesh.animals.model.Animal
+import com.abhinesh.animals.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+    private val listAdapter = AnimalListAdapter(arrayListOf())
+    private val animalListDataObserver = Observer<List<Animal>> {list ->
+        list?.let {
+            animalList.visibility = View.VISIBLE
+            listAdapter.updateAnimalList(it)
+        }
+    }
+
+    private val loadingLiveDataObserver = Observer<Boolean> {isLoading ->
+        loadingView.visibility = if (isLoading) View.VISIBLE else View.GONE
+        if (isLoading) {
+            listError.visibility = View.GONE
+            animalList.visibility = View.GONE
+        }
+    }
+
+    private val errorLiveDataObserver = Observer<Boolean> {isError ->
+            listError.visibility = if (isError) View.VISIBLE else View.GONE
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +45,24 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.animals.observe(viewLifecycleOwner, animalListDataObserver)
+        viewModel.loading.observe(viewLifecycleOwner, loadingLiveDataObserver)
+        viewModel.loadError.observe(viewLifecycleOwner, errorLiveDataObserver)
+        viewModel.refresh()
+
+        animalList.apply {
+            layoutManager = GridLayoutManager(context,2)
+            adapter = listAdapter
+        }
+
+    }
+
+    //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //        super.onViewCreated(view, savedInstanceState)
 //        buttonDetail.setOnClickListener {
 //            val action = ListFragmentDirections.actionListFragmentToDetailFragment()
